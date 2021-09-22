8ea1c3012d011a126d7071e119ce9325cef71f9b
soyowp <soyowp@gmail.com>
2021-09-22 15:24:08 +0900

    kkh - 마이페이지 세션값 인식, 식단출력 추가

--- on_ffice/src/main/java/org/office/controller/MyPageController.java
+++ on_ffice/src/main/java/org/office/controller/MyPageController.java
@@ -9,32 +9,52 @@
 */
 package org.office.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.office.domain.MyPageVO;
 import org.office.service.MyPageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
-//@Controller //컴포넌트스캔
-//@Log4j
-//@RequestMapping("/mypage/*") //이 클래스를 사용하는 메서드이 들어오는 주소 앞 /mypage로 명명
-//@AllArgsConstructor // 의존성 주입
+@Controller // 컴포넌트스캔
+@Log4j
+@RequestMapping("/mypage/*") // 이 클래스를 사용하는 메서드이 들어오는 주소 앞 /mypage로 명명
+@AllArgsConstructor // 의존성 주입
 public class MyPageController {
-	
+
 	@Autowired
 	private MyPageService service;
-	
-	@PostMapping("/main")
-	public void getInfo(String uid, Model model) {
+
+	@GetMapping("/main")
+	public String getInfo(String uid, String dailymeal, Model model, HttpServletRequest request) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		SimpleDateFormat formatter2 = new SimpleDateFormat("yyyyMMdd");
+		HttpSession session = request.getSession();
+		String login_session_uid = (String) session.getAttribute("login_session");
+		String login_time = formatter.format(session.getCreationTime());
+		dailymeal = formatter2.format(new Date());
+		 
 		log.info("유저 메인페이지 접속");
-		MyPageVO vo = service.getInfo(uid);
+		log.info("세션 아이디 : " + login_session_uid);
+		MyPageVO vo = service.getInfo("test1"); //테스트를 위해 test1삽입 추후 login_session_uid로 변경
+		log.info("받아온 정보 : " + vo);
+		MyPageVO vo2 = service.getMeal(dailymeal);
+		log.info("받아온 정보 : " + vo2);
 		model.addAttribute("info", vo);
+		model.addAttribute("login_time", login_time);
+		model.addAttribute("meal", vo2);
+		return "mypage/main";
 	}
-	
 
 }
--- on_ffice/src/main/webapp/WEB-INF/views/mypage/main.jsp
+++ on_ffice/src/main/webapp/WEB-INF/views/mypage/main.jsp
@@ -13,10 +13,46 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+</style>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
+<div class="container">
+<header>
+    
+    <div class="row">
+        
+        <div class="col-md-1">
+        접속한 시간 : ${login_time }
+        </div>
+        <div class="col-md-3">
+        접속자 : ${info.name }
+        부서 : ${info.dp_code }
+        직급 : ${info.position_code }
+        </div>
+        <div class="col-md-4">
+        ${meal.dailymeal}<br/>
+        식단표<br/>
+        ${meal.MENU1 }<br/>
+        ${meal.MENU2 }<br/>
+        ${meal.MENU3 }<br/>
+        ${meal.MENU4 }<br/>
+        ${meal.MENU5 }
+        </div>
+        <div class="col-md-4">
+        
+        </div>
+    </div>
+</header>
+<main>
 
+</main>
+
+<footer>
+
+</footer>    
+</div>
 </body>
 </html>
