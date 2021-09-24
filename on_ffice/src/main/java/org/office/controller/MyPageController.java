24c925de7c1496d05420221e3f69f4a20c72e598
soyowp <soyowp@gmail.com>
2021-09-24 18:40:36 +0900

    kkh - 모달이랑 싸움중

--- on_ffice/src/main/java/org/office/controller/MyPageController.java
+++ on_ffice/src/main/java/org/office/controller/MyPageController.java
@@ -16,16 +16,20 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.annotations.Param;
 import org.office.domain.MealVO;
 import org.office.domain.TodoVO;
 import org.office.domain.UserVO;
 import org.office.service.MyPageService;
+import org.office.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -38,32 +42,47 @@ public class MyPageController {
 
 	@Autowired
 	private MyPageService service;
+	private UserService user_service;
 
 	@GetMapping("/main")
-	public String getInfo(String uid, String dailymeal, Model model, HttpServletRequest request) {
+	public void getInfo(Model model, HttpServletRequest request) {
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		SimpleDateFormat formatter2 = new SimpleDateFormat("yyyyMMdd");
 		HttpSession session = request.getSession();
-		String login_session_id = (String) session.getAttribute("login_session");
-		String login_session_name = (String) session.getAttribute("name_session");
+		UserVO login_session =  (UserVO) session.getAttribute("login_session");
 		String login_time = formatter.format(session.getCreationTime()); //특정 세션만 골라서 출력하는법 찾아보기.
-		dailymeal = formatter2.format(new Date());
+		String dailymeal = formatter2.format(new Date());
 		 
 		log.info("유저 메인페이지 접속");
-		log.info("세션 아이디 : " + login_session_id);
-		UserVO uservo = service.getInfo("test1"); //테스트를 위해 test1삽입 추후 login_session_uid로 변경
+		log.info("받아온 세션 : " + login_session);
+		UserVO uservo = service.getInfo("test1"); //테스트를 위해 test1삽입 추후 로 변경
 		log.info("받아온 정보 : " + uservo);
 		List<TodoVO> todoList = service.getTodo("test1");
 		log.info("받아온 할일 : " +todoList);
 		MealVO mealvo = service.getMeal(dailymeal);
 		log.info("받아온 정보 : " + mealvo);
+		List<UserVO> userList = user_service.allUserInfo();
+		log.info("전체유저리스트 :" + userList);
 		
 		
 		model.addAttribute("info", uservo);
+		log.info("info전송");
 		model.addAttribute("login_time", login_time);
+		log.info("info전송");
 		model.addAttribute("meal", mealvo);
+		log.info("info전송");
 		model.addAttribute("todoList",todoList);
-		return "mypage/main";
+		log.info("info전송");
+		model.addAttribute("users", userList);
+		log.info("info전송");
 	}
 	
+	@PostMapping("/insertTodo")
+	public String insertTodo
+	(TodoVO vo, HttpServletRequest request, RedirectAttributes rttr, Model model) {	
+		service.insertTodo(vo);
+		log.info(vo);
+		model.addAttribute(vo);
+		return "/mypage/main";
+	}
 }
--- on_ffice/src/main/webapp/WEB-INF/views/mypage/main.jsp
+++ on_ffice/src/main/webapp/WEB-INF/views/mypage/main.jsp
@@ -9,7 +9,6 @@
 body{margin:20px;}
 </style>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
 
 </script>
@@ -34,6 +33,16 @@ body{margin:20px;}
         직급 : ${info.position_code }<br/>
         </div>    
         <div class="col-md-8">
+        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#createTodo">
+ 		TODO생성
+		</button>
+		<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#passTodo">
+  		작업자전환
+		</button>
+		<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#completeTodo">
+  		작업완료
+		</button>
+		
         	<table class="text-center table table-hover">
 				<thead>
 				<tr>
@@ -44,7 +53,7 @@ body{margin:20px;}
 				<th>완료여부</th>
 				</tr>				
 				</thead>
-			<c:forEach items="${todoList}" var="todo" varStatus="td">
+			<c:forEach items="${todoList}" var="todo">
 				<tr>
 				<td>${todo.todo_num }</td>
 				<td>${todo.checker }</td>
@@ -72,10 +81,45 @@ body{margin:20px;}
     </div>
     
 </main>
+<!-- Button trigger modal -->
+
+
+<!-- Modal -->
+        <form action="/mypage/insertTodo" method="post">
+<div class="modal fade" id="createTodo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">TODO를 생성합니다</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      	<div class="modal-body">
+   			<select class="form-select btn btn-secondar">
+  			<option selected>작업자를 선택해주세요</option>
+  				<c:forEach items="${users}" var="users">
+  				<option value="${users.uid}">${users.dp_code}부서 ${users.name }</option>
+  				</c:forEach>
+			</select>
+        <span class="input-group-text">작업 제목</span>
+  		<input type="text" class="form-control" placeholder="작업명 입력" name="todotitle">
+  		<br>
+	  	<span class="input-group-text">작업내용</span>
+  		<textarea class="form-control" placeholder="작업내용 입력" name="todocon"></textarea>
+		</div>
+      </div>
+      <div class="modal-footer">
+        <input type="submit" class="btn btn-primary" value="TODO생성"/>
+      </div>
+    </div>
+  </div>
+        </form>
+</div>
+
+<!-- Modal -->
+
 
 <footer>
 
 </footer>    
-</div>
 </body>
 </html>
