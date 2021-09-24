fd0999d8ce97b716a5b610ab937a929a331a56ae
DESKTOP-C750Q06\dyddn <dyddn30612@naver.com>
2021-09-24 18:22:31 +0900

    byw-dplobby에 부서게시글, 부서회원 불러오는것 성공

--- on_ffice/src/main/java/org/office/controller/CompanyLobbyController.java
+++ on_ffice/src/main/java/org/office/controller/CompanyLobbyController.java
@@ -4,11 +4,13 @@ package org.office.controller;
 import javax.servlet.http.HttpSession;
 
 import org.office.domain.UserVO;
+import org.office.service.DpCommunityService;
+import org.office.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -19,6 +21,11 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class CompanyLobbyController {
 	
+	@Autowired
+	UserService us;
+	
+	@Autowired
+	DpCommunityService dcs;
 
 	@GetMapping("/lobby")
 	public String showlobby(Model model, HttpSession session) {
@@ -35,9 +42,15 @@ public class CompanyLobbyController {
 		return "/company/intro";
 	}
 	
-	@GetMapping("/dp")
-	public String showdp(int dp_code) {
+	@GetMapping("/dplobby")
+	public String showdp(int dp_code, Model model) {
 		
-		return "/commpany/dp?dp_code=" + dp_code;
+		log.info("폼에서 받은 dp_code" + dp_code);
+		log.info("부서리스트 " + us.allUserInfoByDp(dp_code));
+		model.addAttribute("dpu_list", us.allUserInfoByDp(dp_code));
+		
+		log.info("dcslist " + dcs.list());
+		model.addAttribute("dpc_list", dcs.list());	//dpc 부서 커뮤니티
+		return "/company/dplobby";
 	}
 }
