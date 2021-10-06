04f99a5cf8c2f5a1628d0cea94a8905ca98760e4
eastar93 <eastar93@naver.com>
2021-10-06 17:36:59 +0900

    SDK - 부서게시판 댓글 Controller, Mapper, Service 로직 구현

--- on_ffice/src/main/java/org/office/service/DpCReplyService.java
+++ on_ffice/src/main/java/org/office/service/DpCReplyService.java
@@ -8,11 +8,10 @@ public interface DpCReplyService {
 	
 	public void addDpCreply(DpCReplyVO vo);
 	
-	public List<DpCReplyVO> listDpCReply(int dc_num);
-	
+	public List<DpCReplyVO> listDpCReply(int dp_community);
 	
 	public void modifyDpCReply(DpCReplyVO vo);
 	
-	public void removeDpCReply(int dc_num);
+	public void removeDpCReply(int dp_community);
 
 }
