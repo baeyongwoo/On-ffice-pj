873ef79fdaddc21f119f376ff046e8594e53885b
eastar93 <eastar93@naver.com>
2021-10-06 18:33:20 +0900

    SDK - 부서게시판 야크 테스트 완료

--- on_ffice/src/main/java/org/office/service/DpCReplyServiceImpl.java
+++ on_ffice/src/main/java/org/office/service/DpCReplyServiceImpl.java
@@ -5,7 +5,14 @@ import java.util.List;
 import org.office.domain.DpCReplyVO;
 import org.office.mapper.DpCReplyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
 public class DpCReplyServiceImpl implements DpCReplyService {
 
 	@Autowired
